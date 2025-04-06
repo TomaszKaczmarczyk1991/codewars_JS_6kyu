1545fac8383c34c69bdbee98f451e49482a455c2
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-04-06 23:52:24 +0200

    add solution

--- firstDup.js
+++ firstDup.js
@@ -1,3 +1,11 @@
 function firstDup (s) {
-   
+    const seen = new Set();
+    
+    for(const char of s) {
+        if(seen.has(char)) return char;
+        else seen.add(char);
+    }
+    return undefined;
   }
+
+console.log(firstDup('translator'));
