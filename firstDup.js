110d720edf3e8277c4743b0905139a7ae57a5a4d
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-04-07 00:25:55 +0200

    fix solution that pass the tests

--- firstDup.js
+++ firstDup.js
@@ -1,11 +1,13 @@
-function firstDup (s) {
-    const seen = new Set();
-    
-    for(const char of s) {
-        if(seen.has(char)) return char;
-        else seen.add(char);
+function firstDup(s) {
+    let j; 
+    for (let i = 0; i < s.length; i++) {
+        let firstChar = s[i];
+        
+        for (j = i + 1; j < s.length; j++) {
+            if (firstChar === s[j]) {
+                return firstChar;
+            }
+        }
     }
     return undefined;
-  }
-
-console.log(firstDup('translator'));
+}
