7d1e6722bdcb69b4f63675e52e2c1e82ad9647be
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-01 13:29:34 +0100

    Add console.log to see counted characters in the object

--- duplicateCount.js
+++ duplicateCount.js
@@ -7,7 +7,10 @@ function duplicateCount(text){
     counts[ch] = (counts[ch] || 0) + 1;
     if(counts[ch] === 2) duplicates++;
   }
-  return duplicates;
+// console.log(counts) // { a: 2, b: 2, c: 1, d: 1, e: 1 }
+  
+return duplicates;
 }
 
 console.log((duplicateCount("aabbcde"))) // 2
+
