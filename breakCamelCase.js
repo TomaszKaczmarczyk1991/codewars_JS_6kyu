623da5c7240bf422013870a96990c2f9a45b498c
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-29 15:52:57 +0100

    add breakCamelCase.js

---
+++ breakCamelCase.js
@@ -0,0 +1,10 @@
+function solution(string) {
+  const result = []
+  for(let i = 0; i < string.length; i++){
+    if(string[i] === (string[i]).toUpperCase()) result.push(' ')
+    result.push(string[i])
+  }
+  return result.join('');
+}
+
+console.log(solution('camelCasing')) // camel Casing
