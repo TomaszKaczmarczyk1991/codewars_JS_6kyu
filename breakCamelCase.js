c557bf3b17e58f45a407b7d37b265dafa8d4967b
TomaszKaczmarczyk1991 <tomaszkaczmarczyk@tuta.io>
2025-11-29 15:53:59 +0100

    remove unnecessary parentheses

--- breakCamelCase.js
+++ breakCamelCase.js
@@ -1,7 +1,7 @@
 function solution(string) {
   const result = []
   for(let i = 0; i < string.length; i++){
-    if(string[i] === (string[i]).toUpperCase()) result.push(' ')
+    if(string[i] === string[i].toUpperCase()) result.push(' ')
     result.push(string[i])
   }
   return result.join('');
